86a273b7cfe1c57d7804652d5b948286a799508f
Daniel E. Doherty <ded-law@ddoherty.net>
2017-04-20 15:09:03 -0500

    Add module-level constructors to build tables.
    Suggested by Avdi Grimm. These just bring existing FatTable::Table constructors
    up to the FatTable module level so we can say, e.g., FatTable.from_csv_file(...)
    instead of FatTable::Table.from_csv_file(...), a convenience for the user of the
    library.

--- lib/fat_table.rb
+++ lib/fat_table.rb
@@ -12,4 +12,82 @@ module FatTable
   require 'fat_table/table'
   require 'fat_table/formatters'
   require 'fat_table/db_handle'
+
+  ###########################################################################
+  # Table Constructors
+  ###########################################################################
+
+  # Return an empty FatTable::Table object.  You can use add_row or add_column
+  # to fill on the table.
+  def self.new
+    Table.new
+  end
+
+  # Construct a FatTable::Table from the contents of a CSV file. Headers will be
+  # taken from the first row and converted to symbols.
+  def self.from_csv_file(fname)
+    Table.from_csv_file(fname)
+  end
+
+  # Construct a FatTable::Table from a string, treated in the same manner as if
+  # read the input from a CSV file. Headers will be taken from the first row and
+  # converted to symbols.
+  def self.from_csv_string(str)
+    Table.from_csv_string(str)
+  end
+
+  # Construct a FatTable::Table from the first table found in the given Emacs
+  # org-mode file. Headers are taken from the first row if the second row is an
+  # hrule. Otherwise, synthetic headers of the form :col_1, :col_2, etc. are
+  # created. Any other hlines will be treated as marking a boundary in the
+  # table.
+  def self.from_org_file(fname)
+    Table.from_org_file(fname)
+  end
+
+
+  # Construct a FatTable::Table from the first table found in the given string,
+  # treated in the same manner as if read from an Emacs org-mode file. Headers
+  # are taken from the first row if the second row is an hrule. Otherwise,
+  # synthetic headers of the form :col_1, :col_2, etc. are created. Any other
+  # hlines will be treated as marking a boundary in the table.
+  def self.from_org_string(str)
+    Table.from_org_string(str)
+  end
+
+  # Construct a FatTable::Table from an array of arrays. By default, with hlines
+  # false, do not look for separators, i.e. nil or a string of dashes, just
+  # treat the first row as headers. With hlines true, expect separators to mark
+  # the header row and any boundaries. If the second element of the array is a
+  # nil, or an array whose first element is a string that looks like an hrule,
+  # '|-----------', '+----------', etc., interpret the first element of the
+  # array as a row of headers. Otherwise, synthesize headers of the form :col_1,
+  # :col_2, ... and so forth. The remaining elements are taken as the body of
+  # the table, except that if an element of the outer array is a nil or an array
+  # whose first element is a string that looks like an hrule, mark the preceding
+  # row as a boundary. In org mode code blocks, by default (:hlines no) all
+  # hlines are stripped from the table, otherwise (:hlines yes) they are
+  # indicated with nil elements in the outer array.
+  def self.from_aoa(aoa, hlines: false)
+    Table.from_aoa(aoa, hlines: hlines)
+  end
+
+  # Construct a FatTable::Table from an array of hashes, or an array of any
+  # objects that respond to the #to_h method. All hashes must have the same
+  # keys, which, converted to symbols, will become the headers for the Table.
+  def self.from_aoh(aoh)
+    Table.from_aoh(aoh)
+  end
+
+  # Construct a FatTable::Table from another FatTable::Table. Inherit any group
+  # boundaries from the input table.
+  def self.from_table(table)
+    Table.from_table(table)
+  end
+
+  # Construct a Table by running a SQL query against the database set up with
+  # FatTable.set_db.  Return the Table with the query results as rows.
+  def self.from_sql(query)
+    Table.from_sql(query)
+  end
 end
--- spec/fat_table_spec.rb
+++ spec/fat_table_spec.rb
@@ -1,7 +1,70 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe FatTable do
-  it "has a version number" do
+  it 'has a version number' do
     expect(FatTable::VERSION).not_to be nil
+  end
+
+  it 'can invoke new constructor' do
+    expect(FatTable.new.class).to eq(FatTable::Table)
+  end
+
+  it 'can invoke from_csv_file constructor' do
+    fname = "#{__dir__}/example_files/wpcs.csv"
+    expect(FatTable.from_csv_file(fname).class)
+      .to eq(FatTable::Table)
+  end
+
+  it 'can invoke from_csv_string constructor' do
+    str = <<-EOS
+Ref,Date,Code,RawShares,Shares,Price,Info
+1,2006-05-02,P,5000,5000,8.6000,2006-08-09-1-I
+2,2006-05-03,P,5000,5000,8.4200,2006-08-09-1-I
+3,2006-05-04,P,5000,5000,8.4000,2006-08-09-1-I
+EOS
+    expect(FatTable.from_csv_string(str).class).to eq(FatTable::Table)
+  end
+
+  it 'can construct from_org_file' do
+    fname = "#{__dir__}/example_files/goldberg.org"
+    expect(FatTable.from_org_file(fname).class)
+      .to eq(FatTable::Table)
+  end
+
+  it 'can construct from_org_string' do
+    str = <<-EOS
+#+CAPTION: Goldberg
+#+ATTR_LATEX: :font \footnotesize
+#+NAME: goldberg
+|     Ref |       Date | Code |   Shares | Price | Info |
+|---------+------------+------+----------+-------+------|
+| 2841381 | 2016-11-04 | P    |   2603.0 |  6.46 |      |
+| 2841382 | 2016-11-04 | S    |   3800.0 |  6.45 |      |
+| 2841500 | 2016-11-04 | P    |   3100.0 |  6.55 |      |
+EOS
+    expect(FatTable.from_org_string(str).class)
+      .to eq(FatTable::Table)
+  end
+
+  it 'can construct from array of arrays' do
+    aoa = [[:a, :b, :c], [1, 2, 3], [4, 5, 6]]
+    expect(FatTable.from_aoa(aoa).class)
+      .to eq(FatTable::Table)
+    aoa_h = [[:a, :b, :c], nil, [1, 2, 3], [4, 5, 6]]
+    expect(FatTable.from_aoa(aoa_h, hlines: true).class)
+      .to eq(FatTable::Table)
+  end
+
+  it 'can construct from array of hashes' do
+    aoh = [{ a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 }]
+    expect(FatTable.from_aoh(aoh).class)
+      .to eq(FatTable::Table)
+  end
+
+  it 'can construct from Table' do
+    aoh = [{ a: 1, b: 2, c: 3 }, { a: 4, b: 5, c: 6 }]
+    tab = FatTable.from_aoh(aoh)
+    expect(FatTable.from_table(tab).class)
+      .to eq(FatTable::Table)
   end
 end
