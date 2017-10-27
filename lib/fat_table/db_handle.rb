717bf9573c79a9546be0908ef268f66c5b6defc5
Daniel E. Doherty <ded-law@ddoherty.net>
2017-10-27 06:52:48 -0500

    Replace references to DBI with Sequel and allow use of Sequel.connect.
    Rather than construct a Sequel connection string, allow the user to just set
    FatTable.db to a connection built by Sequel.connect or driver-specific Sequel
    connection methods.

--- lib/fat_table/db_handle.rb
+++ lib/fat_table/db_handle.rb
@@ -1,18 +1,25 @@
 module FatTable
   class << self;
-    # The +DBI+ database handle returned by the last successful call to
-    # FatTable.set_db.
+    # The +Sequel+ database handle to use in calls to +FatTable.from_sql+.
     attr_accessor :handle
   end
 
-  # This method must be called before calling FatTable.from_sql or
-  # FatTable::Table.from_sql in order to specify the database to use.  All of
-  # the keyword parameters have a default except +database:+, which must contain
-  # the name of the database to query.
+  # This method must be called before calling +FatTable.from_sql+ or
+  # +FatTable::Table.from_sql+ in order to specify the database to use.
+  #
+  # You can pass in a +Sequel+ connection with +db+, or have fat_table construct
+  # a uri from given components. In the latter case, all of the keyword
+  # parameters have a default except +database:+, which must contain the name of
+  # the database to query.
+  #
+  # +db+::
+  #    Inject a Sequel connection constructed +Sequel.connect+ or one of
+  #    Sequel's adapter-specific connection methods.
+  #    http://sequel.jeremyevans.net/rdoc/files/doc/opening_databases_rdoc.html
   #
   # +driver+::
-  #    One of 'Pg' (for Postgresql), 'Mysql' (for Mysql), or 'SQLite3' (for
-  #    SQLite3) to specify the +DBI+ driver to use.  You may have to install the
+  #    One of 'pg' (for Postgresql), 'mysql' or 'mysql2' (for Mysql), or 'sqlite' (for
+  #    SQLite3) to specify the +Sequel+ driver to use.  You may have to install the
   #    driver to make this work.  By default use 'Pg'.
   #
   # +database+::
@@ -20,11 +27,11 @@ module FatTable
   #
   # +user+::
   #    The user name to use for accessing the database.  It defaults to nil,
-  #    which may be interpreted as a default user by the DBI driver being used.
+  #    which may be interpreted as a default user by the Sequel driver being used.
   #
   # +password+::
   #    The password to use for accessing the database.  It defaults to nil,
-  #    which may be interpreted as a default password by the DBI driver being used.
+  #    which may be interpreted as a default password by the Sequel driver being used.
   #
   # +host+::
   #    The name of the host on which to look for the database connection,
@@ -38,44 +45,59 @@ module FatTable
   #    The socket to use to access the database if the host is 'localhost'.
   #    Defaults to the standard socket for the Pg driver, '/tmp/.s.PGSQL.5432'.
   #
-  # If successful the database handle for DBI is return.
+  # If successful the database handle for Sequel is return.
   # Once called successfully, this establishes the database handle to use for
   # all subsequent calls to FatTable.from_sql or FatTable::Table.from_sql.  You
   # can then access the handle if needed with FatTable.db.
-  def self.set_db(driver: 'postgres',
+  def self.set_db(db: nil,
+                  driver: 'postgres',
                   database:,
                   user: ENV['LOGNAME'],
                   password: nil,
                   host: 'localhost',
                   port: '5432',
                   socket: '/tmp/.s.PGSQL.5432')
-    raise UserError, 'must supply database name to set_db' unless database
+    if db
+      self.handle = db
+    else
+      raise UserError, 'must supply database name to set_db' unless database
 
-    valid_drivers = ['postgres', 'mysql', 'sqlite']
-    unless valid_drivers.include?(driver)
-      raise UserError, "'#{driver}' driver must be one of #{valid_drivers.join(' or ')}"
-    end
-    if database.blank?
-      raise UserError, "must supply database parameter to set_db"
-    end
-    pw_part = password ? ":#{password}" : ''
-    hst_part = host ? "@#{host}" : ''
-    prt_part = port ? ":#{port}" : ''
-    dsn = "#{driver}:://#{user}#{pw_part}#{hst_part}#{prt_part}/#{database}"
+      valid_drivers = ['postgres', 'mysql', 'mysql2', 'sqlite']
+      unless valid_drivers.include?(driver)
+        raise UserError, "'#{driver}' driver must be one of #{valid_drivers.join(' or ')}"
+      end
+      if database.blank?
+        raise UserError, "must supply database parameter to set_db"
+      end
 
-    # Set the dsn for DBI
-    begin
-      # DB = Sequel.connect(dsn)
-      self.handle = Sequel.connect(dsn)
-    rescue => ex
-      raise TransientError, "#{dsn}: #{ex}"
+      if driver == 'sqlite'
+        dsn = "sqlite://#{database}"
+      else
+        pw_part = password ? ":#{password}" : ''
+        hst_part = host ? "@#{host}" : ''
+        prt_part = port ? ":#{port}" : ''
+        dsn = "#{driver}:://#{user}#{pw_part}#{hst_part}#{prt_part}/#{database}"
+      end
+
+      # Set the dsn for Sequel
+      begin
+        # DB = Sequel.connect(dsn)
+        self.handle = Sequel.connect(dsn)
+      rescue => ex
+        raise TransientError, "#{dsn}: #{ex}"
+      end
     end
     handle
   end
 
-  # Return the +DBI+ database handle as returned by the last call to
-  # FatTable.set_db.
+  # Return the +Sequel+ database handle.
   def self.db
     handle
+  end
+
+  # Directly set the db handle to a Sequel connection formed without
+  # FatTable.set_db.
+  def self.db=(db)
+    self.handle = db
   end
 end
