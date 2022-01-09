26159e281ddb46dfafb859990055757ade91d9fe
Daniel E. Doherty <ded-law@ddoherty.net>
2022-01-09 04:38:57 -0600

    Change location of rspec persistence file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,7 +52,7 @@ RSpec.configure do |config|
   # Allows RSpec to persist some state between runs in order to support
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.example_status_persistence_file_path = ".rspec_status"
 
 
 # The settings below are suggested to provide a good initial experience
